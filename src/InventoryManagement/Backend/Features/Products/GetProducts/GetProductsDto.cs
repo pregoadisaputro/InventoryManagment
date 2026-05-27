4328c2d0ab64aa325bbfae872cc148aebe651d94
anlucys <pregoadisaputroo@gmail.com>
2026-05-27 15:24:35 +0700

    feat: implement get products pages and summary dto

---
+++ src/InventoryManagement/Backend/Features/Products/GetProducts/GetProductsDto.cs
@@ -0,0 +1,17 @@
+namespace Backend.Features.Products.GetProducts;
+
+public record GetProductsDto(int PageNumber = 1, int PageSize = 5, string? Name = null);
+
+public record ProductPageDto(int TotalPages, IEnumerable<ProductSummaryDto> Data);
+
+public record ProductSummaryDto(
+    int Id,
+    string Name,
+    string Sku,
+    string Category,
+    int Quantity,
+    int StockThreshold,
+    decimal Price,
+    DateTime CreatedAt,
+    string LastUpdatedBy
+);
