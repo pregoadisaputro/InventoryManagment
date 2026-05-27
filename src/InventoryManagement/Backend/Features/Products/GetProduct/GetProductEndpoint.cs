77ed563471f4a4b6fc4be4479fb6db52835c5d50
anlucys <pregoadisaputroo@gmail.com>
2026-05-27 18:04:20 +0700

    feat: implement get specific product endpoint

---
+++ src/InventoryManagement/Backend/Features/Products/GetProduct/GetProductEndpoint.cs
@@ -0,0 +1,39 @@
+using Backend.Data;
+using Backend.Features.Products.Constants;
+using Backend.Features.Products.GetProducts;
+
+namespace Backend.Features.Products.GetProduct;
+
+public static class GetProductEndpoint
+{
+    public static void MapGetProduct(IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+                "/{id:int}",
+                async (int id, InventoryDbContext dbContext) =>
+                {
+                    var existingItems = await dbContext.Products.FindAsync(id);
+
+                    if (existingItems is null)
+                    {
+                        return Results.NotFound();
+                    }
+
+                    var response = new ProductDetailsDto(
+                        existingItems.Id,
+                        existingItems.Name,
+                        existingItems.Sku,
+                        existingItems.CategoryId,
+                        existingItems.Quantity,
+                        existingItems.StockThreshold,
+                        existingItems.Price,
+                        existingItems.CreatedAt,
+                        existingItems.LastUpdatedBy
+                    );
+
+                    return Results.Ok(response);
+                }
+            )
+            .WithName(EndpointNames.GetProduct);
+    }
+}
--- src/InventoryManagement/Backend/Features/Products/GetProducts/GetProductsDto.cs
+++ src/InventoryManagement/Backend/Features/Products/GetProducts/GetProductsDto.cs
@@ -2,16 +2,4 @@ namespace Backend.Features.Products.GetProducts;
 
 public record GetProductsDto(int PageNumber = 1, int PageSize = 5, string? Name = null);
 
-public record ProductPageDto(int TotalPages, IEnumerable<ProductSummaryDto> Data);
-
-public record ProductSummaryDto(
-    int Id,
-    string Name,
-    string Sku,
-    string CategoryName,
-    int Quantity,
-    int StockThreshold,
-    decimal Price,
-    DateTime CreatedAt,
-    string LastUpdatedBy
-);
+public record ProductPageDto(int TotalPages, IEnumerable<ProductDetailsDto> Data);
